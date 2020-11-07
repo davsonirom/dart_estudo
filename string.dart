193de1a22b1d58e4ea764b69922819714fc6b845
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 16:49:59 -0300

    declarando string

---
+++ string.dart
@@ -0,0 +1,13 @@
+
+var nome = 'Lisa';
+// ou
+var nome = "Lisa";
+
+// estao sendo declaradas de forma generica porem estar claro que
+// se trada de um texto.
+
+// entao ao inves de declara uma variavel generica podemos declara-la com seu
+// tipo definido.
+
+String nome = 'Lisa'; // >>> variavel declarada de forma explicita.
+
