78850d3a2392269ddba2acb6d6ad0979bdc431d5
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 19:08:00 -0300

    Operador Relacional

---
+++ operadoresRelacionais.dart
@@ -0,0 +1,27 @@
+
+
+var diga = "Oi!"// = é um operador de atribuição, onde Oi! foi atribuido 
+                // a variavel diga.
+
+int a = 10;
+int b = 8;
+
+print(a == b) // == operador de relacional
+// saida: false
+// ..* o numero 10 não é igual ao numero 8, então é falso
+// .... um resultado booleans = false
+
+// todos as verificações de relação sempre ira retorna um booleans
+// true (verdadeiro) ou false (falso) dependendo o tipo de verificação
+// a ser realizada.
+
+/* todos os operadores relacionais
+
+> maior que
+< menor que
+>= maior ou igual a 
+<= menor ou igual a 
+!= negação do igual
+
+*/
+
