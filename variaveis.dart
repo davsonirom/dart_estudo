b013187a44b42c48940cedf7910909b7da2afd70
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 16:34:57 -0300

    declaracao de variaveis

---
+++ variaveis.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  var nome = 'Lisa';
+  var sobreNome = 'Dias';
+  print(nome);
+  // saida: Lisa
+
+  print('Ola!! eu sou ${nome}');
+  // saida: Ola!! eu sou Lisa
+
+  print(nome + sobreNome);
+  // saida: LisaDias
+
+  print(nome + ' ' + sobreNome);
+  // saida: Lisa Dias
+  
+  print('${nome} ${sobreNome}');
+  // saida: Lisa Dias
+
+}
