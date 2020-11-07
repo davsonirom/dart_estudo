c734e16746066b0d38ab24ec9c9c81ac9cf79b5e
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 18:11:51 -0300

    declarando booleans

---
+++ booleans.dart
@@ -0,0 +1,14 @@
+//  verdadeiro ou falso
+
+bool maiorEIdade = true; // >>> verdadeiro
+
+bool passouDeAno = false; // >>>> falso
+
+/* basicamente informa se algo é verdadeiro ou falso, este conceito fica
+melhor compreendido quando se trabalha com controle de fluxo. 
+
+de forma abstrata..
+se algo for verdade faça isso, senão, então faça isso daqui.
+
+*/
+
