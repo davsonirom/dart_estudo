b94b1154274818d12100773ade56fecfd3fd0c81
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 22:24:59 -0300

    loops for

---
+++ loops.dart
@@ -0,0 +1,24 @@
+// interações de (repetições)
+
+print('Oi!!');
+print('Oi!!');
+print('Oi!!');
+print('Oi!!');
+print('Oi!!');
+print('Oi!!');
+
+//saida:  Oi!!
+          Oi!!
+          Oi!!
+          Oi!!
+          Oi!!
+          Oi!!
+
+
+// quando se tem a quantidade exata de repetições desejada podemos utilizar o
+// for fazer as interações
+
+for ( var i = 0; i <= 6; i ++ ){
+  print('Oi!!');
+}
+// teremos a mesma saida
