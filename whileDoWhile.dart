e61678d86a4c00bfae4c9c255b76c5e35e751610
Davson Oldac Damasceno Machado <davsonirom@me.com>
2020-11-07 22:43:42 -0300

    while do while

---
+++ whileDoWhile.dart
@@ -0,0 +1,21 @@
+// laços de repetição
+
+// while >>> enquanto
+
+int contador; // >>>>> contador de controle do while
+while( contador < 7 ){
+  print('ola!!');
+  contador += 1;
+}
+// o codigo é execultado somente depois da verificação da condição
+
+
+
+// do while >>> ate quando
+int contador; 
+do {
+  print('Oi!!!')
+  contador += 1;
+} while ( contador < 7)
+// deferente-se ao while ja que o  do do-while é excultado uma vez para depois ser
+// realizada a verificação.
